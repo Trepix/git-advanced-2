8c9ad52505dc91655b2665d0c4190c6addcafcf8
ptrepat <ptrepat@externos.grupogodo.com>
2016-11-04 12:00:15 +0100

    Shortened fibonacci algorithm

--- src/main/java/com/grupogodo/corp/training/Fibonacci.java
+++ src/main/java/com/grupogodo/corp/training/Fibonacci.java
@@ -3,20 +3,6 @@ package com.grupogodo.corp.training;
 
 public class Fibonacci {
     public long calculate(int n) {
-        if (n < 2) return n;
-        else return calculateForNBiggerThan2(n);
-    }
-    
-    private long calculateForNBiggerThan2(int n) {
-        long current = 1;
-        long next = 1;
-        
-        for (int i=0; i < n-1; ++i) {
-            long aux = current;
-            current = next;
-            next += aux;
-        }
-        
-        return current;
+        return n < 2 ? n : calculate(n - 1) + calculate(n - 2);  
     }
 }
